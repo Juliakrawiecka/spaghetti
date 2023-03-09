373e299154e2a0f200ae24f0784a369476c5d470
Juliakrawiecka <125580449+Juliakrawiecka@users.noreply.github.com>
2023-03-09 15:39:54 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,49 +1,15 @@
-imie = "jan"
+#zmienna data typu str 17-02-2023
+b="09-03-2023"
+#b=input("Podaj date w formacie dd-mm-rrrr")
+d= b[:2]
+m= b[2:6]
+r= b[-4:]
+ndata= r+m+d
+print(ndata)
 
-print(bool("")) #zwraca false
-print(bool("tomek")) #zwraca true
-
-if imie: #imie jest truthy
-    print("imie istnieje")
-
-successful = True
-for number in range(3):
-    print("Attempt")
-    if successful:
-        print("Successful")
-        break #wyjście z pętli
-
-successful = False
-for number in range(3):
-    print("Attempt")
-    if successful:
-        print("Successful")
-        break
-else:
-    print("Attempted 3 times and failed")
-
-for x in range(5): #x do pieciu czyli od 0 do 4
-    for y in range(3): #y od 0 do 2
-        print(f"({x}, {y})") #wydrukowac wspolrzedne
-
-#iterable
-for x in ("Phyton"):
-    print(x)
-
-#number = 100
-#while number > 0:
-   # print(x//2) #coś tutaj jeszcze
-count=0
-for x in range(1,10):
-    if x %2 == 0:
-        count += 0
-        print(x // 2) == 0
-
-def nazwafunkcji():
-    print("Hi there")
-    print("Welcome abroad")
-nazwafunkcji()
-
-def dodawanie(x,y): #ilość zmiennych musi byc rowna ilosci tego tam 2 i 3 czyli ich jest dwa tak jak dwa sa x i y ##aby wiecej dodaj w def =0
-    print(x + y)
-dodawanie(2,3)
+ld=b.split("-")
+print(ld)
+print(type(ld)) #lista
+lnd=ld[::-1]
+print(lnd) #lista od tyłu
+print("-".join(lnd)) #elementy z listy. join laczy i zamienia w stringa
