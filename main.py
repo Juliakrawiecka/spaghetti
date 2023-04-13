7bea0292f0052b859c372d955fa24bc555d97043
Juliakrawiecka <125580449+Juliakrawiecka@users.noreply.github.com>
2023-04-13 16:25:24 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,74 @@
-#zmienna data typu str 17-02-2023
-b="09-03-2023"
-#b=input("Podaj date w formacie dd-mm-rrrr")
-d= b[:2]
-m= b[2:6]
-r= b[-4:]
-ndata= r+m+d
-print(ndata)
+a = "My jestesmy krasnoludki, hopsasa, hopsasa"
+b =  a[3:11]
+print("b ma wartość:" + b)
+c = a[-7:]
+print("Zmienna c ma wartość:" + c )
 
-ld=b.split("-")
-print(ld)
-print(type(ld)) #lista
-lnd=ld[::-1]
-print(lnd) #lista od tyłu
-print("-".join(lnd)) #elementy z listy. join laczy i zamienia w stringa
+d="my"
+e="KRASNOLUDKI"
+
+e = e.lower()
+print(e)
+
+d = d.capitalize()
+print(d)
+
+print(d + " " + b + " " + e + ", " + c + ", " + c )
+
+#zad2
+
+I="Rwdogrbnrimlnvsspponbnf"
+N="gClkjrvrtudrusjfdoveseh"
+
+Wi = (I[::3])
+print(Wi)
+
+Wn = (N[1::4])
+print(Wn)
+
+print(Wi + " " + Wn)
+
+#3
+print("liczba s:" + str(a.count("s"))) #STR TO TEKST
+print("liczba a:" + str(a.count("a")))
+
+#4
+p = "Pxrxogxxrxamxoxwaxnixxexx xtxo xcxzexxscx ixnfxoxxrxxmxaxtxyxkxxixxx."
+print(p.replace("x", ""))
+
+#5
+u = float(input("Podaj jakąś liczbę: "))
+o = float(input("Podaj jakąś liczbę: "))
+S = u + o
+print("Suma tych liczb wynosi: " + str(S) )
+print("różnica tych liczb wynosi: " + str(u-o) )
+print("potęga tych liczb wynosi: " + str(u*u*u) )
+K = u%o
+print("reszta tych liczb wynosi: " + str(K) )
+
+#7
+I=1
+V=5
+X=10
+L=50
+C=100
+D=500
+M=1000
+g = X + V
+suma = 10 + 5
+print(g)
+
+
+
+
+
+
+
+
+
+
+
+
+#print(a[0:8])
+#print(a[-5:]) koniec
+
